eadbc4cbc88467d8feb1b819fd4fb5e53c351dc4
Ketrel <ketrel@vauge.app>
2024-08-22 17:28:35 -0400

    tabs to spaces

--- MountOff-Redux/core.lua
+++ MountOff-Redux/core.lua
@@ -4,26 +4,26 @@ function SlashCmdList.MOUNTOFF (args)
         SendSystemMessage("/mountoff [help] - without parameters: mounts the same mount as the target, if available.  With the 'help' parameter: prints this message.")
         return
     end
-	if UnitIsPlayer("target") then
-		local buff = nil
-		local mount = nil
-		for i = 1,40 do
+    if UnitIsPlayer("target") then
+        local buff = nil
+        local mount = nil
+        for i = 1,40 do
             buff = C_TooltipInfo.GetUnitBuff("target",i)
-			if buff and buff.id then
-				mount = C_MountJournal.GetMountFromSpell(buff.id)
-				if mount and C_MountJournal.GetMountUsabilityByID(mount, true) then
-					C_MountJournal.SummonByID(mount)
-					break
+            if buff and buff.id then
+                mount = C_MountJournal.GetMountFromSpell(buff.id)
+                if mount and C_MountJournal.GetMountUsabilityByID(mount, true) then
+                    C_MountJournal.SummonByID(mount)
+                    break
                 elseif mount and C_MountJournal.GetMountUsabilityByID(mount, false) then
                     print("You can ride the same mount as your target... if you go outside.")
                     break
-				elseif mount then
-					print("Target is riding a mount you cannot use.")
+                elseif mount then
+                    print("Target is riding a mount you cannot use.")
                     break
-				end
-			end
-		end
-	end
+                end
+            end
+        end
+    end
 --    local id = C_MountJournal.GetMountIDs();
 --    for i = 1, 40 do
 --        local b = C_TooltipInfo.GetUnitBuff("target", i);
